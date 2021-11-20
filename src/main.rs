c882feaf060c143614afbe19427aec51addd7627
norun111 <u.t012807@gmail.com>
2021-11-20 20:52:47 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::env;
+
 mod omikuji;
 
 fn main() {
-    omikuji::run()
+    let args: Vec<String> = env::args().collect();
+    let select: &str;
+    if args.len() > 1 {
+        select = &args[1]
+    } else {
+        select = "nothing"
+    }
+    match select {
+        "omikuji" => omikuji::run(),
+        _ => println!("Something else!"),
+    }
 }
